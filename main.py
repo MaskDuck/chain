f191193183db75ee0a0f073e9898c2054cc4bce9
Minh Dương <edogawaconan0405detective@gmail.com>
2022-01-04 08:19:09 +0700

    typo

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def set(ctx, chain_channel: discord.TextChannel):
         data = {'_id': ctx.guild.id, 'channel': chain_channel.id}
         mongoClient.insert_one(data)
     else:
-        mongoClient.upldate_one({'_id': ctx.guild.id}, {'$set': {'channel': chain_channel.id}})
+        mongoClient.update_one({'_id': ctx.guild.id}, {'$set': {'channel': chain_channel.id}})
     await ctx.send(f'The chain channel has been set to {chain_channel.mention}')
 bot.load_extension('jishaku')
 bot.run(os.environ['ChainBotToken'])
